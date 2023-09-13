975e940719d53deaaa5b92ff09eaa00619ee5fa5
HarmonySpace <jchavaspace@gmail.com>
2023-09-13 10:12:56 -0600

    remove google autentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  pages: {
-    "/google1f5e7bd48e4034a6.html": "src/pages/google1f5e7bd48e4034a6.html",
-  },
-});
+export default defineConfig({});
