e28518721b2041d35be27082650971a27cacac2d
HarmonySpace <jchavaspace@gmail.com>
2023-09-13 10:05:24 -0600

    astro config for google autentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  pages: {
+    "/google1f5e7bd48e4034a6.html": "src/pages/google1f5e7bd48e4034a6.html",
+  },
+});
